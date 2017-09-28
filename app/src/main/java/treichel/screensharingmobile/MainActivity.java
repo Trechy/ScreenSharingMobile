5d20929ee6f67ad3423d7d8d1895f3032068b04b
Brian Treichel <brian_treichel4@outlook.com>
2017-09-28 13:33:30 -0400

    Added a number size checker

--- app/src/main/java/treichel/screensharingmobile/MainActivity.java
+++ app/src/main/java/treichel/screensharingmobile/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity
     implements OnClickListener {
 
     private Button addContactPageButton;
+    private EditText numberText1;
+    private EditText numberText2;
+    private TextView numberOutputText;
+    private Button checkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,39 @@ public class MainActivity extends AppCompatActivity
         addContactPageButton = (Button)
                 findViewById(R.id.addContactPageButton);
         addContactPageButton.setOnClickListener(this);
+
+        numberText1 = (EditText)
+                findViewById(R.id.numberText1);
+        numberText2 = (EditText)
+                findViewById(R.id.numberText2);
+        numberOutputText = (TextView)
+                findViewById(R.id.numberOutputText);
+        checkButton = (Button)
+                findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(this);
+    }
+
+    public String sizeCheck(int number1, int number2){
+        if(number1 > number2){
+            return numberText1.getText() + " is bigger than " + numberText2.getText();
+        }
+        else {
+            return numberText2.getText() + " is bigger than " + numberText1.getText();
+        }
     }
 
     @Override
     public void onClick (View v){
-        Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
-        startActivity(intent);
+        switch(v.getId()){
+            case R.id.addContactButton:
+                Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.checkButton:
+                int number1 = Integer.parseInt(numberText1.getText().toString());
+                int number2 = Integer.parseInt(numberText2.getText().toString());
+                numberOutputText.setText(sizeCheck(number1, number2));
+                break;
+        }
     }
 }
