90a52c4b87c041d204c2caeaaeb454cb4a5f6842
Brian Treichel <brian_treichel4@outlook.com>
2017-09-28 12:51:34 -0400

    Now receive feedback when adding a contact

--- app/src/main/java/treichel/screensharingmobile/AddContactActivity.java
+++ app/src/main/java/treichel/screensharingmobile/AddContactActivity.java
@@ -1,13 +1,41 @@
 package treichel.screensharingmobile;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddContactActivity extends AppCompatActivity {
+public class AddContactActivity extends AppCompatActivity
+    implements View.OnClickListener {
+
+    private EditText contactNameText;
+    private Button addContactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        contactNameText = (EditText)
+                findViewById(R.id.contactNameText);
+        addContactButton = (Button)
+                findViewById(R.id.addContactButton);
+        addContactButton.setOnClickListener(this);
+    }
+
+    public void onClick (View v){
+        String contactName = contactNameText.getText().toString();
+        Context context = getApplicationContext();
+        CharSequence text = contactName + " added";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        contactNameText.setText("");
+        toast.show();
     }
 }
--- app/src/main/java/treichel/screensharingmobile/MainActivity.java
+++ app/src/main/java/treichel/screensharingmobile/MainActivity.java
@@ -1,12 +1,19 @@
 package treichel.screensharingmobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements OnClickListener {
+
+    private Button addContactPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
+
+        addContactPageButton = (Button)
+                findViewById(R.id.addContactPageButton);
+        addContactPageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v){
+        Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+        startActivity(intent);
     }
 }
