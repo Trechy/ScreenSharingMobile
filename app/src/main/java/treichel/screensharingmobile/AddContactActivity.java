2f1b152a4b365e64a98d294a8d5d4baf84b09dce
Brian Treichel <brian_treichel4@outlook.com>
2017-09-27 20:42:14 -0400

    Added the "Add Contact" page

---
+++ app/src/main/java/treichel/screensharingmobile/AddContactActivity.java
@@ -0,0 +1,13 @@
+package treichel.screensharingmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
--- app/src/main/java/treichel/screensharingmobile/MainActivity.java
+++ app/src/main/java/treichel/screensharingmobile/MainActivity.java
@@ -3,6 +3,7 @@ package treichel.screensharingmobile;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         CharSequence text = "Welcome " + username;
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
     }
 }
