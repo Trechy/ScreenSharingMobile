0ee7f45a688b9c6a95e95835d226b0a47459d052
Brian Treichel <brian_treichel4@outlook.com>
2018-01-02 16:57:05 -0500

    Can now change contact avatar to images from the web

--- app/src/main/java/treichel/screensharingmobile/ContactActivity.java
+++ app/src/main/java/treichel/screensharingmobile/ContactActivity.java
@@ -1,15 +1,23 @@
 package treichel.screensharingmobile;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
-import org.w3c.dom.Text;
+import java.io.InputStream;
 
 public class ContactActivity extends AppCompatActivity
         implements OnClickListener {
@@ -18,6 +26,7 @@ public class ContactActivity extends AppCompatActivity
     private TextView statusTextView;
     private Button callButton;
     private Button messageButton;
+    private ImageView avatarImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,9 @@ public class ContactActivity extends AppCompatActivity
         messageButton = (Button)
                 findViewById(R.id.messageButton);
         messageButton.setOnClickListener(this);
+        avatarImageView = (ImageView)
+                findViewById(R.id.avatarImageView);
+        avatarImageView.setOnClickListener(this);
 
 
         Intent intent = getIntent();
@@ -58,6 +70,53 @@ public class ContactActivity extends AppCompatActivity
                 smsIntent.addCategory(Intent.CATEGORY_APP_MESSAGING);
                 startActivity(smsIntent);
                 break;
+            case R.id.avatarImageView:
+                AlertDialog.Builder avatarBuilder = new AlertDialog.Builder(this);
+                avatarBuilder.setTitle("Enter image URL");
+                final EditText imageURL = new EditText(this);
+                imageURL.setInputType(InputType.TYPE_CLASS_TEXT);
+                avatarBuilder.setView(imageURL);
+
+                avatarBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new DownloadImageTask(avatarImageView)
+                                .execute(imageURL.getText().toString());
+                    }
+                });
+                avatarBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                avatarBuilder.show();
         }
+    }
+}
+
+class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
     }
 }
